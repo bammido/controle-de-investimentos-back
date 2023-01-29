3f602c60634ebf53f86b8287baaeaa95a683d09e
Bryan Fernandes <bryanfernandes8@gmail.com>
2023-01-29 16:16:42 -0300

    removendo mongoose

--- index.ts
+++ index.ts
@@ -1,15 +1,8 @@
 import express, { Response, Request } from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
-
-mongoose.connect(`mongodb+srv://bammido:${process.env.DATABASE_PASSWORD}@controle-de-investiment.j0cg3ev.mongodb.net/?retryWrites=true&w=majority`)
-
-const db = mongoose.Connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('connectado!'))
 
 const app = express()
 app.use(express.json())
