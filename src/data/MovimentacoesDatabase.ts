1024c25377ea66249a361439b2557206358a1587
bryanfernandesFrwk <bryanfernandes@frwk.com.br>
2023-02-16 00:17:55 -0300

    userId tabela movimentacao not null

--- src/data/MovimentacoesDatabase.ts
+++ src/data/MovimentacoesDatabase.ts
@@ -28,7 +28,7 @@ export default class MovimentacoesDatabase extends BaseDatabase {
     }
 
     public async createTable(): Promise<void> {
-        const colunas = `papel VARCHAR(10) NOT NULL PRIMARY KEY, dataDaCompra DATE NOT NULL, corretora VARCHAR(50) NOT NULL, preco FLOAT NOT NULL, qtd FLOAT NOT NULL, tipoMovimentacao VARCHAR(10) NOT NULL, userId, FOREIGN KEY (papel) REFERENCES ${(process.env.papeis as string)} (papel), FOREIGN KEY (userId) REFERENCES ${(process.env.usuarios as string)} (id)`
+        const colunas = `papel VARCHAR(10) NOT NULL PRIMARY KEY, dataDaCompra DATE NOT NULL, corretora VARCHAR(50) NOT NULL, preco FLOAT NOT NULL, qtd FLOAT NOT NULL, tipoMovimentacao VARCHAR(10) NOT NULL, userId VARCHAR(255) NOT NULL, FOREIGN KEY (papel) REFERENCES ${(process.env.papeis as string)} (papel), FOREIGN KEY (userId) REFERENCES ${(process.env.usuarios as string)} (id)`
         await super.createTable(colunas)
     }
 
