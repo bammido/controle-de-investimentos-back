1cd98f6ca3a5a20f6067945c65d6fda8f26d9dea
bryanfernandesFrwk <bryanfernandes@frwk.com.br>
2023-02-22 16:04:00 -0300

    editar investimentos

--- src/dropTable.ts
+++ src/dropTable.ts
@@ -1,4 +1,10 @@
 import { movimentacaoDatabase } from "./data/MovimentacoesDatabase";
+import { papelDatabase } from "./data/PapelDatabase";
 import { usuarioDatabase } from "./data/UsuarioDatabase";
 
-movimentacaoDatabase.dropTable()
+async function dropTable() {
+    await movimentacaoDatabase.dropTable()
+    await papelDatabase.dropTable()
+}
+
+dropTable().finally(() => process.exit())
--- src/models/Papel.ts
+++ src/models/Papel.ts
@@ -1,17 +1,13 @@
+import generateUniqueId from "../helpers/functions/generateUniqueId";
+
 export default class Papel {
     constructor(
         public papel: string,
         public nome: string,
         public tipoDeRenda: string,
         public tipoDeInvestimento?: string,
-        public taxasIncidentes?: string
+        public taxasIncidentes?: string,
+        public id: string = generateUniqueId()
     ) { }
 
-    // public getPropriedade(propriedade: any): string {
-    //     return this[propriedade]
-    // }
-
-    // public setPropriedade(propriedade: any, data: string): void {
-    //     this[propriedade] = data
-    // }
 }
