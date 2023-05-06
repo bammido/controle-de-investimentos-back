0854a988da8b36b90f0ed2e00cbfcaa924dd52f4
Bryan Fernandes <bryanfernandes8@gmail.com>
2023-05-06 15:49:10 -0300

    tabela tipo renda

---
+++ src/data/TipoDeRendaDatabase.ts
@@ -0,0 +1,37 @@
+import BaseDatabase from "./BaseDatabase";
+import dotenv from "dotenv"
+import TipoDeRenda from "../models/TipoDeRenda";
+
+dotenv.config()
+
+class TipoDeRendaDatabase extends BaseDatabase {
+    TABLE_NAME = (process.env.TiposDeRenda as string)
+
+    public async getAll(): Promise<TipoDeRenda[]> {
+        return await super.getAll()
+    }
+
+    public async create(novaMovimentacao: TipoDeRenda): Promise<void> {
+        return await super.create(novaMovimentacao)
+    }
+
+    public async findOne(where: any): Promise<any> {
+        return await super.findOne(where)
+    }
+
+    public async dropTable(): Promise<void> {
+        await super.dropTable()
+    }
+
+    public async delete(where: any): Promise<void> {
+        await super.delete(where)
+    }
+
+    public async createTable(): Promise<void> {
+        const colunas = `id VARCHAR(255) NOT NULL PRIMARY KEY, tipo VARCHAR(20) NOT NULL`
+        return await super.createTable(colunas)
+    }
+
+}
+
+export const tipoDeRendaDatabase = new TipoDeRendaDatabase()
